f4e32e509f29b5e054f41d54e068a171d0065cf5
GnivethaG <gnivetha2001@gmail.com>
2023-11-07 23:28:16 +0530

    s

---
+++ candyproblem.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+    int ne,i,sum=0,target=18;
+    printf("Enter the number of employees");
+    scanf("%d",&ne);
+    int a[ne];
+    printf("Enter the candies sold by each employee");
+    for(i=1;i<=ne;i++)
+    {
+        scanf("%d",&a[i]);
+        sum=sum+a[i];
+    }
+
+    for(i=1;i<=ne;i++)
+    {
+        if(sum>18)
+        {
+            a[i]+=2;
+        }
+       printf("\nEmployee %d gets :%d ",i,a[i]*10);
+
+    }
+}
+
+
+
